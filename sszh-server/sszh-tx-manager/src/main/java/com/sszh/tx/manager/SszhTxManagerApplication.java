a4b38b9d1db27b4f8394f932a9f54da1dbc611a3
seesun2012 <seesun2012@qq.com>
2019-07-15 19:42:52 +0800

    集成LCN分布式事务框架

--- sszh-server/sszh-tx-manager/src/main/java/com/sszh/tx/manager/SszhTxManagerApplication.java
+++ sszh-server/sszh-tx-manager/src/main/java/com/sszh/tx/manager/SszhTxManagerApplication.java
@@ -1,11 +1,11 @@
 package com.sszh.tx.manager;
 
+import com.codingapi.txlcn.tm.config.EnableTransactionManagerServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-@EnableEurekaClient
+@EnableTransactionManagerServer
 public class SszhTxManagerApplication {
 
     public static void main(String[] args) {
