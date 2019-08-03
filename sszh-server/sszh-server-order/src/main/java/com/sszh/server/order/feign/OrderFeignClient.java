cfc052a6221c7c62efaa5f8ce4346a6ad92cec56
seesun2012 <seesun2012@qq.com>
2019-08-03 16:08:02 +0800

    集成mybatis框架

--- sszh-server/sszh-server-order/src/main/java/com/sszh/server/order/feign/OrderFeignClient.java
+++ sszh-server/sszh-server-order/src/main/java/com/sszh/server/order/feign/OrderFeignClient.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-@FeignClient(name = "SSZH-SERVER-ORDER", fallbackFactory = FeignClientFallbackFactory.class)
+@FeignClient(name = "SSZH-SERVER-ORDER"/*, fallbackFactory = FeignClientFallbackFactory.class*/)
 public interface OrderFeignClient {
 
     @RequestMapping(value = "/order/{id}", method = RequestMethod.GET)
