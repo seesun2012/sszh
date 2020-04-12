7365d53fac0c0a7d5e82c16c1a8a6a39658eb771
seesun2012 <seesun2012@qq.com>
2020-04-12 15:01:40 +0800

    删除IDEA配置文件

---
+++ sszh-server/sszh-server-mongodb/src/main/java/com/sszh/sszh/server/mongodb/SszhServerMongodbApplication.java
@@ -0,0 +1,13 @@
+package com.sszh.sszh.server.mongodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SszhServerMongodbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SszhServerMongodbApplication.class, args);
+    }
+
+}
